2ffb5271d829243ae1ad92b02f1e9eabe68e0206
Ryan-Vreeke <rvreeke1234@gmail.com>
2024-08-30 20:21:22 -0600

    .gitignore

---
+++ build/CMakeFiles/temple.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temple.dir/src/Watchman.cpp.o"
+  "CMakeFiles/temple.dir/src/Watchman.cpp.o.d"
+  "CMakeFiles/temple.dir/src/WebContext.cpp.o"
+  "CMakeFiles/temple.dir/src/WebContext.cpp.o.d"
+  "CMakeFiles/temple.dir/src/main.cpp.o"
+  "CMakeFiles/temple.dir/src/main.cpp.o.d"
+  "CMakeFiles/temple.dir/src/tmpp.cpp.o"
+  "CMakeFiles/temple.dir/src/tmpp.cpp.o.d"
+  "CMakeFiles/temple.dir/src/webserve.cpp.o"
+  "CMakeFiles/temple.dir/src/webserve.cpp.o.d"
+  "temple"
+  "temple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
